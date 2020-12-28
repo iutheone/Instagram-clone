7a671e0e0bc14e77a0338cf72ba8c6a27e14185e
Ayu_dhawan <adhawan@ee.iitr.ac.in>
2020-12-28 08:25:30 +0530

    changes in post.js

--- models/post.js
+++ models/post.js
@@ -15,7 +15,7 @@ const postSchema = new mongoose.Schema({
     },
     likes:[{type:ObjectId,ref:"User"}],
     comments:[{
-        text:String,
+        type:String,
         postedBy:{type:ObjectId,ref:"User"}
     }],
     postedBy:{
